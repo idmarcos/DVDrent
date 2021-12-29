2dcd1fc1cf5c10af490749d1f82bd21c74fa0e8a
Marcos Iglesias <marcos@vesubio.com>
2021-12-29 13:49:36 +0100

    MIGRACIONES
    Creadas migraciones para las tablas "dvds" y "users_dvds"

---
+++ database/migrations/2021_12_28_155610_create_users_dvds_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersDvdsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users_dvds', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('dvd_id')->constrained();
+            $table->date('rent_date');
+            $table->date('return_date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users_dvds');
+    }
+}
