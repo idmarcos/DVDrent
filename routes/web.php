f9762544d8678a4e2a17c000e39a13674a01d8c3
Marcos Iglesias <marcos@vesubio.com>
2021-12-30 20:44:18 +0100

    MIDDLEWARE ADMIN
    Añadido middleware admin para comprobar acceso a rutas

--- routes/web.php
+++ routes/web.php
@@ -19,59 +19,62 @@ Route::get('/', function () {
     return redirect('login');
 });
 
-Route::prefix('admin')->group(function () {
-    Route::get('/panel', function () {
-        return view('admin.dashboard');
-    })->middleware(['auth'])->name('admin.dashboard');
-});
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+Route::middleware(['auth'])->group(function () {
 
+    Route::middleware(['isAdmin'])->group(function () {
 
-Route::get('/list/movies', [DvdController::class, 'movieList'])
-            ->middleware('auth')
-            ->name('movies.list');
-Route::get('/list/myrents', [DvdController::class, 'userRents'])
-            ->middleware('auth')
-            ->name('user.movies.rent');
+        Route::get('/admin/panel', function () {
+            return view('admin.dashboard');
+        })->name('admin.dashboard');
 
-Route::get('/movies/{id}/rent', [DvdController::class, 'movieFormRent'])
-            ->middleware('auth');
-Route::post('/movies/{id}/rent', [DvdController::class, 'movieRent'])
-            ->middleware('auth')
-            ->name('movies.rent');
-Route::get('/movies/inrent', [DvdController::class, 'moviesInRent'])
-            ->middleware('auth')
+        Route::get('/movies/inrent', [DvdController::class, 'moviesInRent'])
             ->name('movies.inrent');
-Route::get('/movies/returned', [DvdController::class, 'moviesReturned'])
-            ->middleware('auth')
+
+        Route::get('/movies/returned', [DvdController::class, 'moviesReturned'])
             ->name('movies.returned');
 
-Route::post('/rent/{id}/return', [DvdController::class, 'returnMovie'])
-            ->middleware('auth')
-            ->name('movies.return');
-Route::post('/movies/{id}/available', [DvdController::class, 'movieAvailable'])
-            ->middleware('auth')
+        Route::post('/movies/{id}/available', [DvdController::class, 'movieAvailable'])
             ->name('movies.available');
 
-Route::resource('movies', 'App\Http\Controllers\DvdController', [
-    'names' => [
-        'index' => 'movies.index',
-    ]
-])->middleware('auth');
+        Route::resource('movies', 'App\Http\Controllers\DvdController', [
+            'names' => [
+                'index' => 'movies.index',
+            ]
+        ]);
+        
+        Route::resource('clients', 'App\Http\Controllers\ClientController', [
+            'names' => [
+                'index' => 'clients.index',
+            ]
+            ]);
+        
+        Route::resource('sales', 'App\Http\Controllers\SaleController', [
+            'names' => [
+                'index' => 'sales.index',
+            ]
+        ]);
 
-Route::resource('clients', 'App\Http\Controllers\ClientController', [
-    'names' => [
-        'index' => 'clients.index',
-    ]
-])->middleware('auth');
+    });
 
-Route::resource('sales', 'App\Http\Controllers\SaleController', [
-    'names' => [
-        'index' => 'sales.index',
-    ]
-])->middleware('auth');
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+
+    Route::get('/list/movies', [DvdController::class, 'movieList'])
+            ->name('movies.list');
+
+    Route::get('/list/myrents', [DvdController::class, 'userRents'])
+            ->name('user.movies.rent');
+        
+    Route::get('/movies/{id}/rent', [DvdController::class, 'movieFormRent']);
+
+    Route::post('/movies/{id}/rent', [DvdController::class, 'movieRent'])
+            ->name('movies.rent');
+
+    Route::post('/rent/{id}/return', [DvdController::class, 'returnMovie'])
+            ->name('movies.return');
+
+});
 
 require __DIR__.'/auth.php';
